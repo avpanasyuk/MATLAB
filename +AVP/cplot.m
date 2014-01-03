b2c1cc24242713b7426cbb85d1bd2ca536cde2b6
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2014-01-03 15:42:02 -0500

    Added amp and phase plot

--- +AVP/cplot.m
+++ +AVP/cplot.m
@@ -1,4 +1,7 @@
 function cplot(y,x)
   if nargin < 2, x = [1:numel(y)]; end
-  plot(x,real(y),'r-+',x,imag(y),'b-+');
+  plot(x,real(y),'r-.+',x,imag(y),'b-.+');
+  if ~ishold, held = 1; hold on; end
+  plotyy(x,abs(y),x,angle(y));
+  if exist('held'), hold off; end
 end
