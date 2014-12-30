0561ab3323ba6600d78e660816a78bd5cef7eec9
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2014-12-29 20:51:28 -0500

    Minor

--- +AVP/plot_complex.m
+++ +AVP/plot_complex.m
@@ -2,6 +2,7 @@ function plot_complex( y, varargin )
 %PLOT_COMPLEX Summary of this function goes here
 %   Detailed explanation goes here
 
-plot([real(y), imag(y)],varargin{:});
+plot(real(y),varargin{:}); hold on
+plot(imag(y),'.-',varargin{:}); hold off
 end
 
