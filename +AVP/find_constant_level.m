0fab86e9efc61e71270f5b444c8b9b2764f14681
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-02-20 10:36:47 -0500

    Improved find_constant_level.m

--- +AVP/find_constant_level.m
+++ +AVP/find_constant_level.m
@@ -1,17 +1,41 @@
-function [constant error FirstI LastI] = find_constant_level(x, do_plot)
-% if a single continuous interval in data corresponds to
-% constant level we find it.  We go through all values of start and stop of this interval
-% and find minimum error. We assume that its size is > sqrt(numel(x))
+function [constant error FirstI LastI] = find_constant_level(x, do_plot, from_side, min_length)
+%> if a single continuous interval in data corresponds to
+%> constant level we find it.
+%> We go through all values of start and stop of this interval
+%> and find minimum error.
+%> @param from_side, if 1 assumes that constant interval starts in the beginning
+%> if 2 - at the end, if empty - that we have to determine both ends
+%> @param min_length - minimal length of the constant interval, sqrt(numel(x))
+%> by default
 
-if nargin < 2 || isempty(do_plot), do_plot = true; end
 
 n = numel(x);
+
+if nargin < 2 || isempty(do_plot), do_plot = true; end
+if nargin < 3 || isempty(from_side), from_side = 0; end
+if nargin < 4 || isempty(min_length), min_length = fix(sqrt(n)); end
+
 Res = [];
-for FirstI=1:n
-  for LastI=FirstI+fix(sqrt(n)):n
-    Res = [Res; [FirstI,LastI,...
-      mean(x(FirstI:LastI)),std(x(FirstI:LastI))/sqrt(LastI-FirstI)]];
-  end
+switch from_side
+  case 0
+    for FirstI=1:n
+      for LastI=FirstI+min_length:n
+        Res = [Res; [FirstI,LastI,...
+          mean(x(FirstI:LastI)),std(x(FirstI:LastI))/sqrt(LastI-FirstI)]];
+      end
+    end
+  case 1
+    FirstI = 1;
+    for LastI=FirstI+min_length:n
+      Res = [Res; [FirstI,LastI,...
+        mean(x(FirstI:LastI)),std(x(FirstI:LastI))/sqrt(LastI-FirstI)]];
+    end
+  case 2
+    LastI = n;
+    for FirstI=1:n-min_length
+      Res = [Res; [FirstI,LastI,...
+        mean(x(FirstI:LastI)),std(x(FirstI:LastI))/sqrt(LastI-FirstI)]];
+    end
 end
 
 % finding min error
@@ -22,7 +46,7 @@ LastI = Res(minI,2);
 
 if do_plot
   plot(x,'.'); hold on
-  plot([LastI,FirstI],[constant,constant],'r'); hold off 
+  plot([LastI,FirstI],[constant,constant],'r'); hold off
 end
 end
 
