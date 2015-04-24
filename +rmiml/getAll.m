db583618ff768ff339a35e350b66dfeaffd1674f
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-04-24 07:57:04 -0400

    Trying to fix error messages in MATLAB due to ProjectManager

---
+++ +rmiml/getAll.m
@@ -0,0 +1,5 @@
+function varargout = getAll(varargin)
+for n = 1:nargout
+varargout{n} = false;
+end
+end
---
+++ +rmiml/visibleInToolstrip.m
@@ -0,0 +1,4 @@
+function varargout = visibleInToolstrip(varargin)
+for n = 1:nargout
+varargout{n} = false;
+end
