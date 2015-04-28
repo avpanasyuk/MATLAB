2ed57dcfe838cbf8a8faf26e99ac3e360bec2f3e
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-04-28 09:00:48 -0400

    Removed unused stuff

--- +rmiml/getAll.m
+++
@@ -1,5 +0,0 @@
-function varargout = getAll(varargin)
-for n = 1:nargout
-varargout{n} = false;
-end
-end
--- +rmiml/visibleInToolstrip.m
+++
@@ -1,4 +0,0 @@
-function varargout = visibleInToolstrip(varargin)
-for n = 1:nargout
-varargout{n} = false;
-end
