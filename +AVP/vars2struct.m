67ec352bc76922ed7ce6e34515ef830eadf0f53d
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-05-02 12:28:28 -0400

    Modified vars2struct.m

--- +AVP/vars2struct.m
+++ +AVP/vars2struct.m
@@ -1,4 +1,9 @@
-function out = vars2struct(varargin)
-for ni=1:nargin
-  out.(varargin{ni}) = evalin('caller',varargin{ni});
+%> assigns listed variables into fileds of the struct with the same name
+%> @param varargin - cell array of variable names
+function vars2struct(struct_name,varargin)
+  for ni=1:numel(varargin)
+    if ~strcmp(varargin{ni},struct_name) % do not need self assignment
+      evalin('caller',[struct_name '.' varargin{ni} '=' varargin{ni} ';']);
+    end
+  end
 end
