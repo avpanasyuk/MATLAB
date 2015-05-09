bc1592f0467b380ab35b0005b74c428f88415eb2
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-05-09 10:05:58 -0400

    Added AoS2SoA.m

---
+++ +AVP/AoS2SoA.m
@@ -0,0 +1,11 @@
+function out = AoS2SoA(x)
+  names = fieldnames(x);
+  for fi=1:numel(names)
+    if isnumeric(x(1).(names{fi})) && numel(x(1).(names{fi})) == 1
+      out.(names{fi}) = reshape([x.(names{fi})],size(x));
+    end
+  end
+end
+
+      
+      
