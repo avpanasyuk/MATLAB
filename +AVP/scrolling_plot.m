70766764c2b1cebcb1d81cb2b4dcab6f96b91656
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-06-11 14:31:23 -0400

    Refixed scrolling plot

--- +AVP/scrolling_plot.m
+++ +AVP/scrolling_plot.m
@@ -3,6 +3,7 @@
 classdef scrolling_plot < handle
   properties (SetAccess=protected,GetAccess=public)
     % user parameters
+    do_abs = false
     plot_names = {}
     x_npoints = 1000
     plot_props = {}
@@ -10,6 +11,7 @@ classdef scrolling_plot < handle
     period = 0.1 % seconds
     % service
     fig
+    subplots = []
     data_y = []
     data_x = []
     last_x = 0
@@ -19,13 +21,13 @@ classdef scrolling_plot < handle
     function a=scrolling_plot(options)
       a.fig = figure('DeleteFcn',@(varargin) a.delete,'BusyAction','cancel',...
         'Interruptible','off');
-      
       if exist('options','var')
         if isfield(options,'plot_names'), a.plot_names = options.plot_names; end
         if isfield(options,'x_npoints'), a.x_npoints = options.x_npoints; end
         if isfield(options,'same_plot'), a.same_plot = options.same_plot; end
         if isfield(options,'plot_props'), a.plot_props = options.plot_props; end
         if isfield(options,'period'), a.period = options.period; end
+        if isfield(options,'do_abs'), a.do_abs = options.do_abs; end
       end
       a.next_plot = cputime;
     end
@@ -37,34 +39,31 @@ classdef scrolling_plot < handle
     end
     
     function plot(a)
-      old_gcf = gcf;
-      set(0,'CurrentFigure',a.fig);
-      if a.same_plot || size(a.data_y,2) == 1
-        if isreal(a.data_y),
-          plot(a.data_x,a.data_y,'XDataSource','a.data_x',...
-            'YDataSource','a.data_y',a.plot_props{:});
-        else
-          plot(a.data_x,real(a.data_y),a.data_x,imag(a.data_y),'.-',...
-            'XDataSource','a.data_x','YDataSource','a.data_y',a.plot_props{:});
-        end
-      else
+      if isempty(a.data_y), return; end
+      if isempty(a.subplots),
+        % create plot structure
+        old_gcf = gcf;
+        set(0,'CurrentFigure',a.fig);
         for vi=1:size(a.data_y,2)
-          subplot(size(a.data_y,2),1,vi)
-          if isreal(a.data_y),
-            plot(a.data_x,a.data_y(:,vi),'XDataSource','a.data_x',...
-              'YDataSource','a.data_y',a.plot_props{:,vi});
-          else
-            plot(a.data_x,real(a.data_y(:,vi)),a.data_x,imag(a.data_y(:,vi)),'.-',...
-              'XDataSource','a.data_x','YDataSource',...
-              ['a.data_y(:,' num2str(vi) ')'],a.plot_props{:,vi});
+          if a.same_plot, a.subplots(vi) = gca;
+          else a.subplots(vi) = subplot(size(a.data_y,2),1,vi);
           end
-          if ~isempty(a.plot_names),
-            set(gca,'ylabel',a.plot_names{vi});
-          end
+        end
+        set(0,'CurrentFigure',old_gcf);
+      end
+      
+      for vi=1:size(a.data_y,2)
+        if isreal(a.data_y),
+          plot(a.subplots(vi),a.data_x,a.data_y(:,vi),a.plot_props{:,vi});
+        else
+          plot(a.subplots(vi),a.data_x,real(a.data_y(:,vi)),...
+            a.data_x,imag(a.data_y(:,vi)),'.-',a.plot_props{:,vi});
+        end
+        if ~isempty(a.plot_names),
+          set(a.subplots(vi),'ylabel',a.plot_names{vi});
         end
       end
-      set(0,'CurrentFigure',old_gcf);
-    end % first_plot
+    end % plot
     
     function AddPoints(a,y,x)
       if nargin < 3,
@@ -72,9 +71,10 @@ classdef scrolling_plot < handle
         a.last_x = x(end);
       end
       
-      a.data_y = [a.data_y;y];
+      if a.do_abs a.data_y = [a.data_y;abs(y)]; 
+      else a.data_y = [a.data_y;y]; end
       
-      a.data_x = [a.data_x;x];
+      a.data_x = [a.data_x;x(:)];
       if size(a.data_y,1) > a.x_npoints,
         a.data_y = a.data_y(end-a.x_npoints+1:end,:);
         a.data_x = a.data_x(end-a.x_npoints+1:end,:);
@@ -90,7 +90,7 @@ classdef scrolling_plot < handle
     function reset(a)
       a.data_y = [];
       a.data_x = [];
-    end      
+    end
   end %methods
 end % classdef scrolling_plot
 
