b588221175147c6ca490c80a5a94e415e08ff6af
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-10-12 11:15:03 -0400

    Minor fixes

--- +AVP/rms.m
+++ +AVP/rms.m
@@ -1,3 +1,1 @@
-function o = rms(x)
-  o = sqrt(mean(x.^2));
-end
+function o = rms(x), o = sqrt(mean(x.^2)); end
--- finish.m
+++ finish.m
@@ -1,6 +1,8 @@
 if exist('PROJECT_DIR','var')
   h = matlab.desktop.editor.getAll;
   f = fopen([PROJECT_DIR '\matlab.files'],'wt');
-  fprintf(f,'%s\n',h(~strcmp({h.Filename},'Untitled')).Filename);
-  fclose(f);
+  if f ~= -1
+    fprintf(f,'%s\n',h(~strcmp({h.Filename},'Untitled')).Filename);
+    fclose(f);
+  end
 end
