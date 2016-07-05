b8d832d6fce0f58ea204c7bae449706a8544aa85
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2016-07-04 21:13:06 -0400

    Implemented DFT on non-uniformly distributed data

---
+++ +AVP/dft.m
@@ -0,0 +1,41 @@
+function [ft intercept n] = dft(x,y,n,approach)
+  %> this function does fourier transform for non-uniformly distributed samples
+  %> @param x should be in domain [-pi, pi]
+  %> @param y
+  %> @param n - maximum harmonics - number of wavelength over the domain
+  intercept = [];
+  
+  if ~exist('approach','var'), approach = 2; end
+  switch approach
+    case 1
+      SumSin = zeros(1,n); SumCos = zeros(1,n);
+      
+      for HarmI=1:n
+        SumSin(HarmI) = SumSin(HarmI) + sum(sin(HarmI*x).*y);
+        SumCos(HarmI) = SumCos(HarmI) + sum(cos(HarmI*x).*y);
+      end
+      ft = complex(SumCos,SumSin)/numel(x)*2;
+    case 2
+      % let's try to fit
+      Nvect = [0:n];
+      [Narr, Xarr] = meshgrid(Nvect,x);
+      Arg = Narr.*Xarr;
+      Mat = [cos(Arg),sin(Arg(:,2:end))];
+      Sol = pinv(Mat)*y;
+      if max(Sol(2:end)) > (max(y)-min(y))*2
+        error('Two many harmonis, try to decrease n, maybe significantly')
+      end
+      % Sol = y\Mat;
+      ft = complex(Sol(2:n+1),Sol(n+2:end));
+      intercept = Sol(1);
+    case 3
+      % let's try to fit
+      Nvect = [0:n-1];
+      [Narr, Xarr] = meshgrid(Nvect,x);
+      Arg = Narr.*Xarr;
+      Mat = [sin(Arg), cos(Arg)];
+      % Sol = pinv(Mat)*y;
+      Sol = y\Mat;
+      ft = complex(Sol(1:n),Sol(n+1:end));
+  end
+end
