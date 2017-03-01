38e10d6310cf9d6daec54bf47153980301bdb7e5
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2017-02-28 21:19:42 -0500

    Minor fix

--- +AVP/to_bytes.m
+++ +AVP/to_bytes.m
@@ -1,6 +1,8 @@
 function bytes = to_bytes(x)
-  if isreal(x), bytes = typecast(x(:).','uint8');
-  else bytes = [AVP.to_bytes(real(x)),AVP.to_bytes(imag(x))];
+  if isstr(x), bytes = uint8(x); else
+    if isreal(x), bytes = typecast(x(:).','uint8');
+    else bytes = [AVP.to_bytes(real(x)),AVP.to_bytes(imag(x))];
+    end
   end
 end
 
