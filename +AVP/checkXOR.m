607f8055dc59db225b1c293868366d07ed2f3f9f
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2017-07-25 10:14:08 -0400

    XOR implemented in C counts from 11111..., do the same in MATLAB

--- +AVP/checkXOR.m
+++ +AVP/checkXOR.m
@@ -1,4 +1,9 @@
-function out=checkXOR(x)
-  out = x(1); out(1) = 0;
+function out=checkXOR(x,type)
+  %> @param type - sting, only unsigned
+  if ~exist('type','var'), type = class(x(1)); end
+  out = typecast(cast(-1,type(2:end)),type); % generate 1111111...
+  % we select 111111 as start value so sequence of 0s does not
+  % create valid checkXOR value
+  x = typecast(x,type);
   for n=1:numel(x), out = bitxor(out,x(n)); end
 end
