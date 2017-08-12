4944cd5d0016797353c469bdecb32cdc43a2a21f
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2017-08-12 11:43:05 -0400

    Minor

--- +AVP/erase.m
+++ +AVP/erase.m
@@ -1,5 +1,5 @@
 function erase(var_name)
   %> deletes and cleans variable
-  evalin('caller',['if exist(''' var_name ''',''var''), delete(' var_name ...
+  evalin('caller',['if exist(''' var_name ''',''var'') && isobject(' var_name '), delete(' var_name ...
     '); clear(''' var_name '''); end']);
 end
