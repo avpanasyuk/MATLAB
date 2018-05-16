350510c5a04c291d0ac33ffbf7b2d32025a53ef3
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2018-05-16 12:06:17 -0400

    Minor

--- startup.m
+++ startup.m
@@ -1,36 +1,49 @@
-% ok, let's find project root directory which should be somewhere  up the tree. 
-global PROJECT_DIR REP_ROOT DATA_DIR
-PROJECT_DIR = pwd; PROJECT_DIR(1) = upper(PROJECT_DIR(1)); 
-REP_ROOT = PROJECT_DIR; % looking for the root of git repository
-DATA_DIR = 'c:\Dropbox\JEF Core\Sasha\DATA\';
+% ok, let's find project root directory which should be somewhere up the tree.
+global PROJECT_DIR
+PROJECT_DIR = pwd; PROJECT_DIR(1) = upper(PROJECT_DIR(1));
+CurDir = PROJECT_DIR;
 
-while ~isempty(REP_ROOT)
-	if exist([REP_ROOT '\MATLAB'],'dir'), break; end
-  REP_ROOT = fileparts(REP_ROOT);
+while ~isempty(CurDir)
+  if exist([CurDir '\MATLAB'],'dir')
+    MATLAB_DIR = [CurDir '\MATLAB'];
+    PROJECT_DIR = CurDir;
+    break
+  end
+  if exist([CurDir '\.git'],'dir')
+    MATLAB_DIR = CurDir;
+    PROJECT_DIR = CurDir;
+    break
+  end
+  CurDir = fileparts(CurDir)
 end
 
-if isempty(REP_ROOT)
-  error('Can not find repository root which has MATLAB subdirectory!')
-end
-    
-addpath([REP_ROOT '\MATLAB'], [REP_ROOT '\MATLAB\AVP_LIB'],  [REP_ROOT '\MATLAB\AVP_LIB\ARESLab']);
+addpath(MATLAB_DIR, [MATLAB_DIR '\AVP_LIB']);
+run mystartup.m
 
-
-% SET OLD PLOT PALETTE
-co = [0 0 1;
-      0 0.5 0;
-      1 0 0;
-      0 0.75 0.75;
-      0.75 0 0.75;
-      0.75 0.75 0;
-      0.25 0.25 0.25];
-set(groot,'defaultAxesColorOrder',co)
+% SET OLD PLOT PALETTE,BUT WITH CLEAR SEQUENCE RGBCMYKG
+co = [1.00 0.00 0.00;
+  0.00 0.50 0.00;
+  0.00 0.00 1.00;
+  0.00 0.75 0.75;
+  0.75 0.00 0.75;
+  0.50 0.50 0.00;
+  0.00 0.00 0.00;
+  0.75 0.75 0.75];
+% SET IMPROVED COLORS
+% co = CONTRIB.brewermap(8,'Accent');
+set(0,'defaultAxesColorOrder',co)
 
 AVP.clearvars()
+format compact
+format shortg
+% set(0,'defaulttextinterpreter','none')
+set(0,'DefaultFigureWindowStyle','docked')
 
 
 
-  
-  
-  
-  
+
+
+
+
+
+
