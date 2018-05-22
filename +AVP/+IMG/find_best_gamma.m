cbd3f4ca4e09809641e932178b7f0dc85b7fa6ce
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2018-05-22 17:10:50 -0400

    Small fixes

--- +AVP/+IMG/find_best_gamma.m
+++ +AVP/+IMG/find_best_gamma.m
@@ -1,6 +1,6 @@
 function [img gamma] = find_best_gamma(img)
   %> finds best gamma and while checking that there is no solarization
-  imgs = AVP.scale_to_range(img,[0,1]);
+  imgs = AVP.scale_to_range(double(img),[0,1]);
   gamma = fzero(@(x) mean(imgs(:).^x)-0.5,[0,10],optimset('Display','iter'));
-  img = img.^gamma;
+  img = imgs.^gamma;
 end
