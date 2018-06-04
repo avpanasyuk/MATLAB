11cf8542abd06d14c3c64e62dd65b3ac238f5a04
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2018-06-04 14:48:51 -0400

    Small improvement

--- +AVP/scale_to_range.m
+++ +AVP/scale_to_range.m
@@ -1,7 +1,11 @@
-function d = scale_to_range(d,range)
+function [d,offset,scale] = scale_to_range(d,range)
+  %. Dout = Din*scale + offset
   %> scales to integer with a given number of bits
   if ~exist('range','var'), range = [0,254.99999]; end
+  if ~isa(d,'double'), d = double(d); end
   mid = min(d(:));
   mad = max(d(:));
-  d = range(1) + double(d-mid)*(range(2)-range(1))/double(mad-mid);
+  scale = (range(2)-range(1))/(mad-mid);
+  offset = range(1) - mid*scale;
+  d = d*scale + offset;
 end
