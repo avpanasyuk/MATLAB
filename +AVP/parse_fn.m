a4992a60fac2d3248686ba717a6563f4f4f09b4f
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2019-06-16 14:32:53 -0400

    Fixing parse_fn.m, textscan is tricky

--- +AVP/parse_fn.m
+++ +AVP/parse_fn.m
@@ -1,15 +1,19 @@
-function [files,values] = parse_fn(scanf_format,varargin)
+function [files,out] = parse_fn(scanf_format,varargin)
   %> @param scan_format - TEXTSCAN format string to parse file names with,
   %>                      SHOULD MATCH THE WHOLE FILE NAME. Items should be
   %>                      separated by underlines (by default)
   %> @param varargin - 'dir' - directory, '.' as default
   %>                   'pattern' - file pattern to match, '*' as default
+  %>                   'delimiter' - string or cell array of strings, to separate strings,
+  %>                                 none as default. If you specify
+  %>                                 delimeter you have to specify every 
+  %>                                 delimited part as a field
   %> @retval files - file names matching scan_format
   %> @retval values - array of parced with scanf_format values
   
   d = AVP.opt_param('dir','.',1);
   pattern = AVP.opt_param('pattern','*',1);
-  delimiter = AVP.opt_param('Delimiter','_',1);
+  delimiter = AVP.opt_param('Delimiter','',1);
   
   t = what();
   old_dir = t.path;
@@ -21,9 +25,18 @@ function [files,values] = parse_fn(scanf_format,varargin)
   out = cellfun(@(str) textscan(str,scanf_format,...
       'Delimiter',delimiter,varargin{:}),files,'UniformOutput',false);
     
-  out1 = reshape([out{:}],[],size(out,2));
-  for valI=size(out1,2):-1:1
-    values{valI} = [out1{valI,:}];
+  out = cat(1,out{:});
+  bad = cellfun(@isempty,out);
+  good_ind = find(sum(bad,2) == 0);
+  files = files(good_ind);
+  values = out(good_ind,:);
+  out = {};
+  for valI=1:size(values,2)
+    if ischar(values{1,valI})
+      out{valI} = values(:,valI);
+    else
+      out{valI} = [values{:,valI}];
+    end
   end
   cd(old_dir);
 end
