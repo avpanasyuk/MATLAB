e0e725df38724c3dbff8b4f8f1c6c6f59f7955b6
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2019-07-21 14:58:49 -0400

    Added remove_outliers.m

---
+++ +AVP/remove_outliers.m
@@ -0,0 +1,17 @@
+function [Out,OutliersInds,Mean,Std] = remove_outliers(x,varargin)
+  AVP.opt_param('Nsigmas',3);
+  
+  [OutliersInds,Mean,Std] = deal([]);
+  Out = x(:);
+  
+  while 1
+    
+  Mean = mean(Out)
+  Std = std(Out)
+  CurBadInds = find(Out > Mean + Nsigmas*Std | Out < Mean - Nsigmas*Std);
+  if isempty(CurBadInds), break; end
+  OutliersInds = [OutliersInds,CurBadInds];
+  Out(CurBadInds) = [];
+  
+  end 
+end
