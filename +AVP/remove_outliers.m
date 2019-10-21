02be2739891a3e25dfa31a8f0ed7529dbd102b23
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2019-10-21 09:12:31 -0400

    Minor fixes

--- +AVP/remove_outliers.m
+++ +AVP/remove_outliers.m
@@ -1,4 +1,5 @@
 function [Out,OutliersInds,Mean,Std] = remove_outliers(x,varargin)
+  % rempves outliers from a vector
   AVP.opt_param('Nsigmas',3);
   
   [OutliersInds,Mean,Std] = deal([]);
@@ -6,12 +7,12 @@ function [Out,OutliersInds,Mean,Std] = remove_outliers(x,varargin)
   
   while 1
     
-  Mean = mean(Out)
-  Std = std(Out)
-  CurBadInds = find(Out > Mean + Nsigmas*Std | Out < Mean - Nsigmas*Std);
-  if isempty(CurBadInds), break; end
-  OutliersInds = [OutliersInds,CurBadInds];
-  Out(CurBadInds) = [];
-  
-  end 
+    Mean = mean(Out);
+    Std = std(Out);
+    CurBadInds = find(Out > Mean + Nsigmas*Std | Out < Mean - Nsigmas*Std);
+    if isempty(CurBadInds), break; end
+    OutliersInds = [OutliersInds,CurBadInds];
+    Out(CurBadInds) = [];
+    
+  end
 end
