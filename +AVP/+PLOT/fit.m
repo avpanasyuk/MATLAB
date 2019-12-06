f5c37b7409ade24290d8fa8f52d56ef4e9e589cd
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2019-12-05 23:25:23 -0500

    Added fitting different stuff to hist.m

--- +AVP/+PLOT/fit.m
+++ +AVP/+PLOT/fit.m
@@ -1,4 +1,4 @@
-function plot_fit(x,y,f,c,c_init),
+function fit(x,y,f,c,c_init),
 % overplots fit function f=@(c,x) over data
 plot(x,y,'.b'); hold on; axis manual
 if nargin > 4, plot(x,f(c_init,x),'-g'); end
--- +AVP/+PLOT/hist.m
+++ +AVP/+PLOT/hist.m
@@ -16,7 +16,8 @@ function [density values ydivs p] = hist(y,varargin)
   AVP.opt_param('nbins',ceil(sqrt(n)));
   AVP.opt_param('show',true);
   AVP.opt_param('plot_logx',false);
-  AVP.opt_param('fit_gauss',false);
+  AVP.opt_param('fitfunc','none'); % possible options "gauss", "xgauss", 
+  % 'x2gauss','xexp','x2exp','x_to_n_exp','x_to_n_gauss'
   
   idivs = round(linspace(1,n,nbins)).';
   y = sort(y);
@@ -26,21 +27,68 @@ function [density values ydivs p] = hist(y,varargin)
   density = (idivs(2:end)-idivs(1:end-1))./(ydivs(2:end)-ydivs(1:end-1));
   values = (ydivs(2:end)+ydivs(1:end-1))/2;
   
-  if fit_gauss
-    core_densI = find(density > max(density)/40);
-    core_val = values(core_densI);
-    p = polyfit(core_val,log(density(core_densI)),2);
-  else p = [];
+  core_densI = find(density > max(density)/40);
+  core_val = values(core_densI);
+  switch(fitfunc)
+    case 'gauss'
+      p = polyfit(core_val,log(density(core_densI)),2);
+      fitd = exp(polyval(p,core_val));
+    case 'xgauss'
+      [Max, MaxI] = max(density);
+      b = sqrt(2)*values(MaxI);
+      a = Max*exp(0.5)*sqrt(2)/b;
+      p = fit(core_val,density(core_densI),'a*x*exp(-(x/b).^2)',...
+        'Start',[a,b]);
+      fitd = p(core_val);
+    case 'x2gauss'
+      [Max, MaxI] = max(density);
+      b = values(MaxI);
+      a = Max*exp(1)/b.^2;
+      p = fit(core_val,density(core_densI),'a*x^2*exp(-(x/b).^2)',...
+        'Start',[a,b]);
+      fitd = p(core_val);
+    case 'xexp'
+      [Max, MaxI] = max(density);
+      b = values(MaxI);
+      a = Max*exp(1)/b;
+      p = fit(core_val,density(core_densI),'a*x*exp(-(x/b))',...
+        'Start',[a,b]);
+      fitd = p(core_val);
+    case 'x2exp'
+      [Max, MaxI] = max(density);
+      b = values(MaxI)/2;
+      a = Max*exp(2)/4/b.^2;
+      p = fit(core_val,density(core_densI),'a*x.^2*exp(-(x/b))',...
+        'Start',[a,b]);
+      fitd = p(core_val);
+    case 'x_to_n_exp'
+      [Max, MaxI] = max(density);
+      c = 1;
+      b = values(MaxI)/c;
+      a = Max*exp(c)/(b*c).^c;
+      p = fit(core_val,density(core_densI),'a*x.^c*exp(-(x/b))',...
+        'Start',[a,b,c]);
+      fitd = p(core_val);
+    case 'x_to_n_gauss'
+      [Max, MaxI] = max(density);
+      c = 1;
+      b = values(MaxI)*sqrt(2/c);
+      a = Max*exp(c/2)/(b*sqrt(c/2)).^c;
+      p = fit(core_val,density(core_densI),'a*x.^c*exp(-(x/b).^2)',...
+        'Start',[a,b,c]);
+      fitd = p(core_val);
+    otherwise
+      p = [];
+      fitd = [];
   end
   if show
     if plot_logx
       semilogx(abs(values),density,'*-');
     else
       plot(values,density,'.-');
-      if fit_gauss
-        fit = exp(polyval(p,core_val));
+      if ~isempty(fitd)
         hold on
-        plot(core_val,fit); hold off
+        plot(core_val,fitd); hold off
       end
     end
   end
