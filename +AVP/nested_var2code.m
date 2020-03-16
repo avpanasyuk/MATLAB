6705121ccc98bbcf8a819004de5ce1efb831919e
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2020-03-16 17:14:36 -0400

    Minor changes

--- +AVP/nested_var2code.m
+++ +AVP/nested_var2code.m
@@ -1,33 +1,39 @@
 function out_str = nested_var2code(var_name)
   % no tables, individual, array, cell array, string and struct
+  % should be called in top level
   endl = char(13);
   x = evalin('caller',var_name);
-  if numel(x) > 1
-    out_str = '';
-    for i=1:numel(x)
-      if iscell(x), index = '{' + num2str(i) + '}';
-      else index = '(' + num2str(i) + ')';
-      end
-      out_str = out_str + AVP.nested_var2code(var_name + index);
-    end
-    out_str = var_name + ' = reshape(' + var_name + ',' + mat2str(size(x)) + ');' + endl; % restore proper dimensions
-  else % single variable
-    switch class(x)
-      case 'char'
-        out_str = var_name + ' = ''' + x + ''';' + endl;
-      case 'struct'
-        fields = fieldnames(x);
-        out_str = '';
-        for i=1:numel(fields),
-          out_str = out_str + AVP.disp_nested_var(var_name + '.' + fields{i});
+  % it may be string
+  if ischar(x) && size(x,1) == 1
+    out_str = [var_name, ' = ''', x, ''';', endl];
+  else
+    if numel(x) > 1
+      out_str = '';
+      for i=1:numel(x)
+        if iscell(x), index = ['{', num2str(i), '}'];
+        else index = ['(', num2str(i), ')'];
         end
-      case 'cell'
-        out_str = AVP.disp_nested_var(var_name + '{1}');
-      otherwise
-        out_str =  var_name + ' = ' + class(x) + '(' + num2str(x) + ');' + endl;
+        out_str = [out_str, evalin('caller',['AVP.nested_var2code(''', var_name, index,''')'])];
+      end
+      out_str = [out_str, var_name, ' = reshape(', var_name, ',', mat2str(size(x)), ');', endl]; % restore proper dimensions
+    else % single variable
+      switch class(x)
+        case 'char'
+          out_str = [var_name, ' = ''', x, ''';', endl];
+        case 'struct'
+          fields = fieldnames(x);
+          out_str = '';
+          for i=1:numel(fields),
+            out_str = [out_str, evalin('caller',['AVP.nested_var2code(''', var_name, '.', fields{i}, ''')'])];
+          end
+        case 'cell'
+          out_str = evalin('caller',['AVP.nested_var2code(''',var_name, '{1}',''')']);
+        otherwise
+          out_str =  [var_name, ' = ', class(x), '(', num2str(x), ');', endl];
+      end
     end
   end
-  
+end  
   
   
   
--- START_and_FINISH/startup.m
+++ START_and_FINISH/startup.m
@@ -1,63 +1,61 @@
 % ok, let's find project root directory which should be somewhere up the tree.
-global PROJECT_DIR MATLAB_DIR
-INIT_DIR = pwd; INIT_DIR(1) = upper(INIT_DIR(1));
-PROJECT_DIR = INIT_DIR;
-CurDir = INIT_DIR;
-
-while ~isempty(CurDir)
-  if exist([CurDir '\MATLAB'],'dir')
-    MATLAB_DIR = [CurDir '\MATLAB'];
-    PROJECT_DIR = CurDir;
-    break
-  end
-  if exist([CurDir '\.git'],'dir')
-    MATLAB_DIR = CurDir;
-    PROJECT_DIR = CurDir;
-    break
-  end
-  CurDir = fileparts(CurDir);
-end
-
-addpath(MATLAB_DIR)
-if exist([MATLAB_DIR '\AVP_LIB'],'dir'), addpath([MATLAB_DIR '\AVP_LIB']); end
-
-try
-run mystartup.m
-catch
-end
-
-%% let's open  files from the last visit. 
-%% Try current directory
-% I do not want to close and open the same file
-if  exist([INIT_DIR '\matlab.files'],'file')
-  MfileDir = INIT_DIR;
-elseif ~strcmpi(MATLAB_DIR, INIT_DIR) && exist([MATLAB_DIR '\matlab.files'],'file')
-  MfileDir = MATLAB_DIR;
-end
-
-if exist('MfileDir','var')
-  h = matlab.desktop.editor.getAll;
-  Filenames = {h.Filename};
-  KeepI = zeros(1,numel(h));
+if (~isdeployed)
   
-  f = fopen([MfileDir '\matlab.files']);
-  while 1
-    l = fgetl(f);
-    if ~ischar(l), break; end
+  global PROJECT_DIR MATLAB_DIR
+  INIT_DIR = pwd; INIT_DIR(1) = upper(INIT_DIR(1));
+  PROJECT_DIR = INIT_DIR;
+  CurDir = INIT_DIR;
+  
+  while ~isempty(CurDir)
+    if exist([CurDir filesep 'MATLAB'],'dir')
+      MATLAB_DIR = [CurDir filesep 'MATLAB'];
+      PROJECT_DIR = CurDir;
+      break
+    end
+    if exist([CurDir filesep '.git'],'dir')
+      MATLAB_DIR = CurDir;
+      PROJECT_DIR = CurDir;
+      break
+    end
+    CurDir = fileparts(CurDir);
+  end
+  
+  addpath(MATLAB_DIR)
+  if exist([MATLAB_DIR filesep 'AVP_LIB'],'dir'), addpath([MATLAB_DIR filesep 'AVP_LIB']); end
+  
+  %% let's open  files from the last visit.
+  %% Try current directory
+  % I do not want to close and open the same file
+  if  exist([INIT_DIR filesep 'matlab.files'],'file')
+    MfileDir = INIT_DIR;
+  elseif ~strcmpi(MATLAB_DIR, INIT_DIR) && exist([MATLAB_DIR filesep 'matlab.files'],'file')
+    MfileDir = MATLAB_DIR;
+  end
+  
+  if exist('MfileDir','var')
+    h = matlab.desktop.editor.getAll;
+    Filenames = {h.Filename};
+    KeepI = zeros(1,numel(h));
     
-    if exist(l,'file')    
-    OpenI = strcmp(l,Filenames);
-    if ~any(OpenI)
-      eval(['edit ''' l '''']);
-    else
-      KeepI(OpenI) = true;
+    f = fopen([MfileDir filesep 'matlab.files']);
+    while 1
+      l = fgetl(f);
+      if ~ischar(l), break; end
+      
+      if exist(l,'file')
+        OpenI = strcmp(l,Filenames);
+        if ~any(OpenI)
+          eval(['edit ''' l '''']);
+        else
+          KeepI(OpenI) = true;
+        end
+      end
     end
-    end
-  end
-  
-  % now we have to close files we do not need
-  h(~KeepI).close;
-  fclose(f);
+    
+    % now we have to close files we do not need
+    h(~KeepI).close;
+    fclose(f);
+  end  
 end
 
 %% SET OLD PLOT PALETTE,BUT WITH CLEAR SEQUENCE RGBCMYKG
@@ -86,6 +84,11 @@ co = [1.00 0.00 0.00;
 set(0,'defaultAxesColorOrder',co)
 
 try, AVP.clearvars(); catch; end
+try
+  run mystartup.m
+catch
+end
+
 format compact
 format shortg
 set(0, 'DefaultTextInterpreter', 'none')
