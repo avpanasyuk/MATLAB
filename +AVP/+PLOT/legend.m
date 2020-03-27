6df5fda4a65dfc6fdca0c9d829be7fafd6669136
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2020-03-27 15:42:17 -0400

    Modified my Relative error definition to 2*(a-b)/sqrt(a^2+b^2) so it is differentiable

--- +AVP/+PLOT/legend.m
+++ +AVP/+PLOT/legend.m
@@ -1,9 +1,15 @@
 % automatically labels lines of current plot with numbers
 function [ax,objs,ploth,texth] = legend(Labels,varargin)
+  AVP.opt_param('LineWidth',4,1);
+  AVP.opt_param('MarkerSize',20,1); 
+  
   n = numel(get(gca,'Children'));
   if ~AVP.is_defined('Labels')
     Labels = cellstr(num2str([1:n].'));
   end
   [ax,objs,ploth,texth] = legend(Labels,varargin{:});
-  [objs(n+1:2:end).LineWidth] = deal(4);
+  for Id = n+1:2:3*n-1
+    objs(Id).LineWidth = LineWidth;
+    objs(Id+1).MarkerSize = MarkerSize;
+  end
 end
--- +AVP/rel_error.m
+++ +AVP/rel_error.m
@@ -1,3 +1,5 @@
-function err = rel_error(x1,x2)
-  err = (x1-x2)./sqrt((x1.^2+x2.^2)/2);
+function [err, weight] = rel_error(x1,x2)
+  % I like this approach, range -2*sqrt(2)..2*sqrt(2)
+  weight = 1./sqrt((x1.*conj(x1)+x2.*conj(x2))/2);
+  err = (x1-x2).*weight;
 end
