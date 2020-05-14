1ecb90ee396b001b396f6bd540b413d81209143c
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2020-05-14 15:51:13 -0400

    Additional parameters

--- +AVP/+PLOT/complex.m
+++ +AVP/+PLOT/complex.m
@@ -1,9 +1,12 @@
-function plot_complex(y,x)
-  y = y(:); 
-  if nargin < 2, x = [1:size(y,1)]; else x = x(:); end
-  plot(x,real(y),'r-.+',x,imag(y),'b-.+');
-  held = ishold;
-  if ~held, hold on; end
-  plotyy(x,abs(y),x,angle(y));
-  if ~held, hold off; end
+function complex(y,varargin)
+  AVP.opt_param('no_abs_phase',0);
+  AVP.opt_param('x',1:numel(y));
+  y = y(:);
+  plot(x,real(y),'r-+',x,imag(y),'b-+');
+  if ~no_abs_phase
+    held = ishold;
+    if ~held, hold on; end
+    plotyy(x,abs(y),x,angle(y));
+    if ~held, hold off; end
+  end
 end
