4d0603dc598be0449bec405365eedb079dbec828
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2020-05-21 16:03:20 -0400

    Real is blue, imag is cyan

--- +AVP/+PLOT/complex.m
+++ +AVP/+PLOT/complex.m
@@ -2,11 +2,14 @@ function complex(y,varargin)
   AVP.opt_param('no_abs_phase',0);
   AVP.opt_param('x',1:numel(y));
   y = y(:);
-  plot(x,real(y),'r-+',x,imag(y),'b-+');
+  plot(x,real(y),'b-+',x,imag(y),'c-+');
+  line_titles = {'real','imag'};
   if ~no_abs_phase
     held = ishold;
     if ~held, hold on; end
     plotyy(x,abs(y),x,angle(y));
     if ~held, hold off; end
+    line_titles = {line_titles{:},'abs','angle'};
   end
+  % AVP.PLOT.legend(line_titles);
 end
