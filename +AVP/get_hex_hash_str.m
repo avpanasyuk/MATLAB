8c82f7c520cff457788aeaeb6083e7bbde8eaf19
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2020-06-12 17:30:13 -0400

    Improved CRC16_CCITT.m

--- +AVP/get_hex_hash_str.m
+++ +AVP/get_hex_hash_str.m
@@ -1,3 +1,3 @@
 function  hex_hash = get_hex_hash_str(anything)
-    hex_hash = dec2hex(AVP.CRC16_CCITT(AVP.CONVERT.something2bytes(anything)));
+  hex_hash = dec2hex(AVP.CRC16_CCITT(AVP.CONVERT.something2bytes(anything)));
 end
