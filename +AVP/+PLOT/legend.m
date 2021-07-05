da26ca2f8096e69aaa39b3a93a2b0ffb06d273b2
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2021-07-05 13:41:10 -0400

    Fix for MATLAB 21

--- +AVP/+PLOT/legend.m
+++ +AVP/+PLOT/legend.m
@@ -1,6 +1,9 @@
 % automatically labels lines of current plot with numbers
 function [ax,objs,ploth,texth] = legend(varargin)
   n = numel(get(gca,'Children'));
-  [ax,objs,ploth,texth] = legend(cellstr(num2str([1:n].')),varargin{:});
+  if numel(varargin{1}) == n, labels = varargin{1}; 
+  else labels = cellstr(num2str([1:n].'));
+  end
+  [ax,objs,ploth,texth] = legend(labels,varargin{2:end});
   [objs(n+1:2:end).LineWidth] = deal(4);
 end
