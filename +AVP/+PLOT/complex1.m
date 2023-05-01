2805663844bf36218b513e71ac3e418a5de9d1ce
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2023-05-01 14:09:01 -0400

    Fixes

--- +AVP/+PLOT/complex1.m
+++ +AVP/+PLOT/complex1.m
@@ -1,6 +1,6 @@
 function complex1(y,varargin)
   %> because scale of all plots is different let's use subplots
-  AVP.opt_param('x',1:numel(y),1);
+  AVP.opt_param('x',1:size(y,1),1);
   AVP.opt_param('funcArr',{@real,@imag,@abs,@angle},1);
   
   for fI=1:numel(funcArr)
