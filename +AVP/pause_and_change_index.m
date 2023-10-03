a069e0674d507f49b3e4aafc58a9aeb390e83ea6
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2023-10-02 21:30:47 -0400

    pause_and_change_index.m returns key as well

--- +AVP/pause_and_change_index.m
+++ +AVP/pause_and_change_index.m
@@ -1,11 +1,15 @@
-function Index = pause_and_change_index(Index,LastValue)
+function [Index, key] = pause_and_change_index(Index,LastValue)
   %> function pauses and waits for user keypress. If it is
   %>  - q :returns LastValue + 1
   %>  - , :(or left arraw) - returns Index - 1 or LastValue if wrapped
   %>  -  everything else : returns Index + 1
+  %> @note: you can not change counter inside FOR-loop, use WHILE
+  %> keys:
+  %> 'p' == 112
   
-  Index = Index + 1;
-  switch AVP.getkey('Prompt','< - previous, q - exit, anything else - next:')
+  Index = Index + 1; % by default go to next step
+  key = AVP.getkey('Prompt','< - previous, q - exit, anything else - next:');
+  switch key
     case {28,44}
       Index = Index - 2; if Index == 0, Index = LastValue; end
     case 113
