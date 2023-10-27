ff1f2465976c22e6aac2ecb79ee686579bce18a6
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2023-10-27 12:41:47 -0400

    write_png.m simplified

--- +AVP/+IMG/write_png.m
+++ +AVP/+IMG/write_png.m
@@ -6,20 +6,22 @@ function write_png(output_file,varargin)
   %> @param varargin - arguments for 'print' function, like '-r200'
   % we are going to make a draft print first, remove empty rows and columns
   % and then make real print.
+  AVO.opt_param('fig',gcf);
   
   % find output name
   if exist('output_file','var')
-    temp_file = [tempname '.png'];
-    print(varargin{:},'-dpng',temp_file);
-    Im = 255 - imread(temp_file); % negative, white is 0
-    % fix empty spaces
-    BG = ~any(Im,3);
-    Im(all(BG,2),:,:) = [];
-    Im(:,find(all(BG,1)),:) = [];
-    [~,~] = mkdir(fileparts(output_file));
-    imwrite(255-Im,output_file,'png');
-    delete(temp_file);
+%     temp_file = [tempname '.png'];
+%     print(varargin{:},'-dpng',temp_file);
+%     Im = 255 - imread(temp_file); % negative, white is 0
+%     % fix empty spaces
+%     BG = ~any(Im,3);
+%     Im(all(BG,2),:,:) = [];
+%     Im(:,find(all(BG,1)),:) = [];
+%     [~,~] = mkdir(fileparts(output_file));
+%     imwrite(255-Im,output_file,'png');
+%     delete(temp_file);
+    imwrite(frame2in(getframe(fig)),output_file,'png');
   else
-    hgexport(gcf,'-clipboard')
+    hgexport(fig,'-clipboard')
   end
 end
