5df70b6a662713475eafbbaed81f8ba9975b8b4e
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2023-11-09 12:20:20 -0500

    AoS2SoA.m can squeeze dimensions

--- +AVP/+CONVERT/AoS2SoA.m
+++ +AVP/+CONVERT/AoS2SoA.m
@@ -1,25 +1,29 @@
-function out = AoS2SoA(x, names)
+function out = AoS2SoA(x, varargin)
   %> converts array of structure into structure of [cell] arrays. If field
   %> size is the same for all structures in array it gets stacked into array
   %> along first unitary dimension, otherwise cell array
-  if ~AVP.is_defined('names'), names = fieldnames(x); end
-  str_sz = size(x);
+  AVP.opt_param('names', fieldnames(x));
+  AVP.opt_param('DoSqueeze',false);
+
+  if numel(x) == 1, out = x; end
+  in_sz = size(x);
+  % strip leading ones
+  % in_sz = in_sz(find(in_sz ~= 1,1,"first"):end);
   for fi=1:numel(names)
     % let's see whether number of rows of this field for every structure is the
     % same, so we can do conversion
-    sz = cellfun(@(x) size(x,2),{x.(names{fi})},'UniformOutput',false);
-    sz_arr = cat(1,sz{:});
-    sz = size(sz_arr);
-    if all(all(sz_arr == repmat(sz_arr(1,:),sz(1),1))) && ...
+    sz1 = size(x(1).(names{fi}));
+    if all(cellfun(@(el) isequal(size(el),sz1),{x.(names{fi})})) && ...
         ~isa(x(1).(names{fi}),'function_handle')
-      c_arr = {x.(names{fi})};
-      dims = size(c_arr{1});
-      stack_dim = find(dims ~= 1,1,'last') + 1;
-      if isempty(stack_dim), stack_dim = 1; end
-      dims(stack_dim:stack_dim+ndims(x)-1) = str_sz;
-      out.(names{fi}) = reshape(cat(stack_dim,c_arr{:}),dims);
+      CollectField = [x.(names{fi})]; % they are all the same size, so they should stack
+      % CollectField possibly lost some dimensions
+      Dims = [sz1,in_sz];
+      if DoSqueeze, Dims(Dims == 1) = []; end 
+      out.(names{fi}) = reshape([x.(names{fi})],[Dims,1]); % they are all the same size, so they should stack
+      % but maybe loose some dimensions, so we restore them
     else
-      out.(names{fi}) = reshape({x.(names{fi})},str_sz);
+      if DoSqueeze, in_sz(in_sz == 1) = []; end 
+      out.(names{fi}) = reshape({x.(names{fi})},[in_sz(:),1]);
     end
   end
 end
