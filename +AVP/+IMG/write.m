d4a9f7101fffb0a8a9089f65a87509662c92e1ed
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2024-02-15 00:08:32 -0500

    Implemented for all formats.

--- +AVP/+IMG/write.m
+++ +AVP/+IMG/write.m
@@ -1,27 +1,18 @@
-function write_png(output_file,varargin)
+function write(output_file,varargin)
   %>same as print only removes empty spaces. Works only for printing into
   %>image files!
   %> @param output_file - file to write. If omitted just copies figure to
   %> clipboard
-  %> @param varargin - arguments for 'print' function, like '-r200'
-  % we are going to make a draft print first, remove empty rows and columns
-  % and then make real print.
-  AVP.opt_param('fig',gcf);
-  AVP.opt_param('format','png');
+  %> @param varargin - arguments for 'imwrite' function
+  AVP.opt_param('fig',gcf,1);
   
   % find output name
   if exist('output_file','var')
-%     temp_file = [tempname '.png'];
-%     print(varargin{:},'-dpng',temp_file);
-%     Im = 255 - imread(temp_file); % negative, white is 0
-%     % fix empty spaces
-%     BG = ~any(Im,3);
-%     Im(all(BG,2),:,:) = [];
-%     Im(:,find(all(BG,1)),:) = [];
-%     [~,~] = mkdir(fileparts(output_file));
-%     imwrite(255-Im,output_file,'png');
-%     delete(temp_file);
-    imwrite(frame2im(getframe(fig)),output_file,format);
+    [filepath,~,ext] = fileparts(output_file);
+    [~,~] = mkdir(filepath);
+    if isempty(ext), ext = 'png'; else ext = strip(ext,"left",'.'); end 
+    AVP.opt_param('frmt',ext,1);
+    imwrite(frame2im(getframe(fig)),output_file,frmt,varargin{:});
   else
     hgexport(fig,'-clipboard')
   end
